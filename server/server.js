5b8505931c6494a4cfb55212bdebfbebfac34c66
Wilfredo D. Catalan <99998916+Wilfred1097@users.noreply.github.com>
2023-12-20 15:18:20 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,6 +9,13 @@ import cookieParser from 'cookie-parser';
 const salt = 10;
 dotenv.config();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://shopay-client.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 const authenticateToken = (req, res, next) => {
   const token = req.cookies.token;
 
